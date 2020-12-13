101b38db68fb8ff8ff8d4eb40ff0a86c21606e22
Luis Dufour <lgudinodufour@gmail.com>
2020-12-13 03:36:44 -0300

    se crean las distintas secciones de la web con los estilos iniciales, faltan imagenes y bg

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,7 +6,7 @@ listaCursos = $("#cursos")
 
 $.ajax({
     method: 'GET',
-    url : "../assets/data/data.json",
+    url : "./data.json",
     datatype: 'json'
 }).done(function(data){
     renderCursos(data);
@@ -18,12 +18,15 @@ $.ajax({
 function renderCursos(data) {
     data.forEach(function(curso, index) {
         listaCursos.append(
-        `<div>
-        <img src="" alt="curso 1">
-        </div>
-        <h3>${curso.title}</h3>
-        <p>${curso.description}</p>
-        <a href="${curso.schedule}" data-id="${index}" class="btn btn-dark">Ver Programa</a>`
+        `<div class="col mb-4" style="padding-top: 15px">
+            <div class="card h-100 border-secondary">
+                <div class="card-body text-center">
+                    <h5 class="card-title">${curso.title}</h5>
+                    <p class="card-text">${curso.description}</p>
+                    <a href="${curso.schedule}" data-id="${index}" class="btn btn-dark">Ver Programa</a>
+                </div>
+            </div>
+        </div>`
         )
     })
-};
+}
